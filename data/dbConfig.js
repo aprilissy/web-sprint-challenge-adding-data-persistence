af3577e4f09f75e2eb1d365720480c145b04a68d
April Darger <april.m.darger@gmail.com>
2020-12-11 08:47:31 -0700

    Basic set up with insalling dependencies and knexfile/dbConfig set up.

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,1 +1,7 @@
-// complete your db configuration
+const knex = require('knex');
+
+const config = require('../knexfile');
+
+const db = knex(config.development); // wselect the development object from our knexfile
+
+module.exports = db; // export for use in codebase
--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,22 @@
+// Update with your config settings.
+
 module.exports = {
+
   development: {
-    // complete your knexfile
-    migrations: {
-      directory: "./data/migrations",
+    client: 'sqlite3', // our DBMS driver
+    connection: {
+      filename: './data/projects.db3', // the location of our db
     },
+    useNullAsDefault: true, // necessary when using sqlite3 to prevent bugs and issues
+    migrations: {
+      directory: './data/migrations' // not required
+    }
   },
+
+  staging: {
+  },
+
+  production: {
+  }
+
 };
