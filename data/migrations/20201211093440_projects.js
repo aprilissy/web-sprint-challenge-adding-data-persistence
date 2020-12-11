5c4c78181fded789c3512c622a96fa5d20b2008f
April Darger <april.m.darger@gmail.com>
2020-12-11 10:00:51 -0700

    Wrote tables in migration.

--- data/migrations/20201211093440_projects.js
+++ data/migrations/20201211093440_projects.js
@@ -1,8 +1,35 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('projects', table => {
+      table.increments('project_id')
+      table.string('project_name', 128).notNullable()
+      table.string('project_description', 128)
+      table.boolean('project_completed').notNullable().defaultTo(0)
+    })
+    .createTable('resources', table => {
+      table.increments('resource_id')
+      table.string('resource_name', 128).notNullable().unique()
+      table.string('resource_description', 128)
+    })
+    .createTable('tasks', table => {
+      table.increments('task_id')
+      table.string('task_description', 128).notNullable()
+      table.string('task_notes', 128)
+      table.boolean('task_completed').notNullable().defaultTo(0)
+    })
+    .createTable('task_resources', table => {
+      table.increments('task_resource_id')
+      table.integer('resource_id').unsigned().references('resource_id').inTable('resources').onDelete('RESTRICT').onUpdate('RESTRICT')
+      table.integer('task_id').unsigned().references('task_id').inTable('tasks').onDelete('RESTRICT').onUpdate('RESTRICT')
+
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('task_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
 };
